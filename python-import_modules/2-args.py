7a4b4fb7e65ae14575682db1e8da5a42b7712250
MikeBilbee <cmbilbee@gmail.com>
2023-05-10 11:49:28 -0700

    2.1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
-    n = len(sys.argv)
-    hank = 0
-    for i in range(1, n):
-        hank += int(sys.argv[i])
-    print("{}".format(hank))
+    cb = len(sys.argv)
+    if cb == 1:
+        print("0 arguments.")
+    elif cb == 2:
+        print("1 argument:\n1: {}".format(str(sys.argv[1])))
+    else:
+        print("{} arguments:".format(cb - 1))
+        for i in range(len(sys.argv)):
+            if i != 0:
+                print("{}: {}".format(i, str(sys.argv[i])))
