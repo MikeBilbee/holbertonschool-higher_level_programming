965e235696c0c54b5e275fa73c59a748275d3036
MikeBilbee <cmbilbee@gmail.com>
2023-05-22 10:46:22 -0700

    0.2

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This class defines a Square"""
 
+
 class Square:
     """This class defines a Square"""
     def __init__(self, size=None):
         """This initializes a square with a size"""
-        self._size = size
+        pass
