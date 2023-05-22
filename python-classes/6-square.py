03630782b6d70bd29b2d50c1ea8593bbd9b3b915
MikeBilbee <cmbilbee@gmail.com>
2023-05-22 11:35:32 -0700

    6.0

---
+++ python-classes/6-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This is the square class!"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if (self.__size <= 0):
+            print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                for j in range(self.position[0]):
+                    print(end=" ")
+                for k in range(self.size):
+                    print("#", end="")
+                print()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
