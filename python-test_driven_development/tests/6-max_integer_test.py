1cace713d9dc58276a8a43aa2ffef9b02bf6588e
MikeBilbee <cmbilbee@gmail.com>
2023-05-23 10:50:44 -0700

    6.0

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer
+"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    unittest class for max int
+    """
+
+    def test_module_docstring(self):
+        """Tests for module docsting"""
+        modstring = __import__('6-max_integer').__doc__
+        self.assertTrue(len(modstring) > 1)
+
+    def test_function_docstring(self):
+        """Tests for function docstring"""
+        funcstring = max_integer.__doc__
+        self.assertTrue(len(funcstring) > 1)
+
+    def test_empty(self):
+        """test if empty list"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_negatives(self):
+        """checks negative numbers"""
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+
+    def test_type(self):
+        """makes sure input is a list"""
+        self.assertIs(list, list)
+
+    def test_invalid(self):
+        self.assertRaises(TypeError, max_integer((6, 7)))
+
+    def test_positive_integers(self):
+        """
+        This function tests max_integer with positive integers.
+        """
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([1, 3, 2]), 3)
+        self.assertEqual(max_integer([3, 1, 2]), 3)
+
+    def test_one_int(self):
+        self.assertEqual(max_integer([1]), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
