653b9fafa6cd5cfc9f1c017dc1ce26dd52eb9f22
MikeBilbee <cmbilbee@gmail.com>
2023-05-23 10:14:14 -0700

    0.0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This adds 2 integers together"""
+
+
+def add_integer(a, b=98):
+    """ Addition Function for Integers """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
