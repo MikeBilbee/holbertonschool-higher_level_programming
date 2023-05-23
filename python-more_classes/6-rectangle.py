6ff58d5782da3e9e30a376fec21b9425bcc2b752
MikeBilbee <cmbilbee@gmail.com>
2023-05-23 11:59:20 -0700

    6.2

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """ A class that defines a Rectangle"""
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """
@@ -17,6 +18,7 @@ class Rectangle:
         """
         self.height = height
         self.width = width
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
