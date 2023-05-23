bdbda9c82a6285fc945a787432f2564e25433d11
MikeBilbee <cmbilbee@gmail.com>
2023-05-23 10:40:14 -0700

    4.0

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Prints "Prints a square"
+"""
+
+
+def print_square(size):
+    """ Checks to make sure size is an int > 0, then prints a square """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
