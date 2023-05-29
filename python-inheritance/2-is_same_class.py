e77bb2e02289e25712d8ab410a66f732e0d535d2
MikeBilbee <cmbilbee@gmail.com>
2023-05-29 10:29:44 -0700

    2.0

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyhton3
+""" Determines if an object is an exact instance of a class """
+
+
+def is_same_class(obj, a_class):
+    """ Return: True if obj is the exact class a_class; Otherwise False """
+    return(type(obj) == a_class)
