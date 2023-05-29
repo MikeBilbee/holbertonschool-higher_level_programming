b027059eb48c5d42a99673ce1f6e1783f4449a52
MikeBilbee <cmbilbee@gmail.com>
2023-05-29 10:46:24 -0700

    6.0

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Class: BaseGeometry """
+
+
+class BaseGeometry:
+    """ Defines attributes for basic geometry """
+    def area(self):
+        """ Assigns Area for geometry """
+        raise Exception("area() is not implemented")
