beef901bb97c52a60bed6c89eb0d8a2a161850e5
MikeBilbee <cmbilbee@gmail.com>
2023-05-30 09:24:48 -0700

    8.1

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ Returns the dictionary description with simple data structure """
 
-import sys
-
 
 def class_to_json(obj):
     """ Returns the dictionary description with simple data structure """
