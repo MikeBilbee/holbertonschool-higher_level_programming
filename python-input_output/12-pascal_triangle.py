2fc6121631dd2ebdb25d9efdbd603cbe76b7539c
MikeBilbee <cmbilbee@gmail.com>
2023-05-30 09:34:39 -0700

    12.0

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """ Returns: list of pascal's Triangle """
+    triangle = []
+    for i in range(n):
+        triangle.append([])
+        for j in range(i + 1):
+            if j == i or j == 0:
+                triangle[i].append(1)
+            else:
+                triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+    return triangle
