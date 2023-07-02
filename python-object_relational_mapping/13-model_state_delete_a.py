fced846d9527771500a96cd48112a763750fc5e6
MikeBilbee <cmbilbee@gmail.com>
2023-07-02 11:49:57 -0700

    13.2 idk why it says module isnt documented

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Deletes all State objects with a name containing the letter a """
+""" 
+Deletes all State with a name containing the letter a 
+"""
 
 import sys
 from model_state import Base, State
