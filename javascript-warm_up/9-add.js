62ac630a0fc9369a789be5a17ff2d6734c98b780
MikeBilbee <cmbilbee@gmail.com>
2023-07-27 10:53:49 -0700

    9.2

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
 
-const add = (a, b) => {
-  return a + b;
-};
+function add (a, b) {
+  const c = Number(a) + Number(b);
+  return c;
+}
 
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
+const a = process.argv[2];
+const b = process.argv[3];
 
-if (!Number.isNaN(arg1) && !Number.isNaN(arg2)) {
-  const result = add(arg1, arg2);
-  console.log(result);
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
+} else {
+  console.log(add(a, b));
 }
