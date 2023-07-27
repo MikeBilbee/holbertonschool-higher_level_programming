63141666cf5a8f8588b74afbf8182cfdb0cfc520
MikeBilbee <cmbilbee@gmail.com>
2023-07-27 10:49:04 -0700

    9.0 adding

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const add = (a, b) => {
+  return a + b;
+};
+
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+if (!Number.isNaN(arg1) && !Number.isNaN(arg2)) {
+  const result = add(arg1, arg2);
+  console.log(result);
+} else {
+  console.log('Invalid input. Please provide two integers.');
+}
