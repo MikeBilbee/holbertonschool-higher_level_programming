bfa7eeb11473d5c7a2651f359fb80a90f3a0ba2c
MikeBilbee <cmbilbee@gmail.com>
2023-07-27 11:42:23 -0700

    10.1

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const factorial = (n) => {
+function factorial (n) {
   if (isNaN(n)) {
     return 1;
   } else if (n === 0) {
@@ -10,6 +10,5 @@ const factorial = (n) => {
   }
 };
 
-const arg = parseInt(process.argv[2]);
-const result = factorial(arg);
-console.log(result);
+const n = parseInt(process.argv[2]);
+console.log(factorial(n));
