7544f144dcd73cfa9b3c9eb6d7efb0fb7455ea52
MikeBilbee <cmbilbee@gmail.com>
2023-07-31 14:45:35 -0700

    4.0 Square with even More stuff

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let xCount = 0;
+    let yCount = 0;
+    let output = '';
+    for (; xCount < this.width; xCount++) {
+      output += 'X';
+    }
+    for (; yCount < this.height; yCount++) {
+      console.log(output);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = Rectangle;
